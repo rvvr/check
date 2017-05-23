8f1f72f75494390e0de83e42bb4913b53ca9d1b8
Vladimir Romanov <git@romanov.work>
2017-05-23 17:34:47 +0500

    Edit design

--- src/components/updates/updates.vue
+++ src/components/updates/updates.vue
@@ -13,10 +13,12 @@
         span.update_discount -{{ update.discount }}%
     template(v-if="key == 'available_hd'")
       .update_title hd buy
-      .update_info Available
+      .update_info ${{update.price}}
+        span.update_discount Available
     template(v-if="key == 'available_sd'")
       .update_title sd buy
-      .update_info Available
+      .update_info ${{update.price}}
+        span.update_discount Available
     template(v-if="key == 'decreased_hd_rent'")
       .update_title hd rent
         span.old_price $ {{ update.last_price }}
@@ -29,14 +31,17 @@
         span.update_discount -{{ update.discount }}%
     template(v-if="key == 'available_hd_rent'")
       .update_title hd rent
-      .update_info Available
+      .update_info ${{update.price}}
+        span.update_discount Available
     template(v-if="key == 'available_sd_rent'")
       .update_title sd rent
-      .update_info Available
+      .update_info ${{update.price}}
+        span.update_discount Available
 </template>
 
 <script>
 export default {
-  name: 'updates', props: ['updates']
+  name: 'updates',
+  props: ['updates', 'prices']
 }
 </script>
