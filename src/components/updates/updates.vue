8bcbf809312468a091a2ba4afc00ccd3c339346f
Vladimir Romanov <git@romanov.work>
2017-05-24 12:10:08 +0500

    Edit movie style

--- src/components/updates/updates.vue
+++ src/components/updates/updates.vue
@@ -14,11 +14,11 @@
     template(v-if="key == 'available_hd'")
       .update_title hd buy
       .update_info ${{update.price}}
-        span.update_discount Available
+        span.update_available Available
     template(v-if="key == 'available_sd'")
       .update_title sd buy
       .update_info ${{update.price}}
-        span.update_discount Available
+        span.update_available Available
     template(v-if="key == 'decreased_hd_rent'")
       .update_title hd rent
         span.old_price $ {{ update.last_price }}
@@ -32,11 +32,11 @@
     template(v-if="key == 'available_hd_rent'")
       .update_title hd rent
       .update_info ${{update.price}}
-        span.update_discount Available
+        span.update_available Available
     template(v-if="key == 'available_sd_rent'")
       .update_title sd rent
       .update_info ${{update.price}}
-        span.update_discount Available
+        span.update_available Available
 </template>
 
 <script>
