fec8d4138a03d71ff468635fb5cc77dce1d7f3ba
Vladimir Romanov <git@romanov.work>
2017-05-24 14:32:13 +0500

    Add loader to subscribe form

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -4,18 +4,22 @@ export default {
   name: 'subscribe',
   data() {
     return {
-      message: false
+      message: false,
+      loading: false
     }
   },
   methods: {
     subscribe() {
       event.target.submit.disabled = true
+      this.loading = true
+      console.log(  this.loading)
       axios.put('/subscription', {
         email: event.target.email.value,
         filter: this.$store.getters.filters
       })
       .then( ({data}) => {
         this.message = data.text
+        this.loading = false
       })
     }
   }
--- src/components/subscribe/subscribe.vue
+++ src/components/subscribe/subscribe.vue
@@ -7,6 +7,7 @@
     form.sub_form(name="subscribe_form" @submit.prevent="subscribe")
         input.sub_email(type="email" name="email" placeholder="Your email" required)
         input.sub_submit(type="submit" value="subscribe" name="submit")
+        .loader(v-if="loading")
   template(v-if="message")
     p.subscribe_desc {{message}}
 </template>
